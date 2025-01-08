90af0aecabad3e95c5cc695bc8c720b83f47f75a
GuihCFerreira <guihhh231@gmail.com>
2025-01-07 22:48:34 -0300

    refactor: ensure decimal warning does not happen

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -16,6 +16,9 @@ const TransactionsPage = async () => {
     where: {
       userId,
     },
+    orderBy: {
+      date: "desc",
+    },
   });
 
   const userCanAddTransaction = await canUserAddTransaction();
